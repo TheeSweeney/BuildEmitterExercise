b3af262ac612c1531b976f044d984b8407a8272b
Brian Sweeney <bsweeney930@gmail.com>
2017-06-14 00:22:46 -0400

    return JSON

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var fs = require('fs')
 
 http.createServer(function(req, res) {
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.createReadStream(__dirname + '/index.html').pipe(res)
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var obj = {
+    firstname: 'Brian',
+    lastname: 'Sweeney'
+  }
+  res.end(JSON.stringify(obj))
 
 
  
