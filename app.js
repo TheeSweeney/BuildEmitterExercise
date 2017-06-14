bc350fafe7f4fa331b9579ec31019254c22bae92
Brian Sweeney <bsweeney930@gmail.com>
2017-06-14 01:02:09 -0400

    npm conceptual test

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-var http = require('http');
-var fs = require('fs')
-
-http.createServer(function(req, res) {
-
-  if(req.url === '/'){
-    fs.createReadStream( __dirname + '/index.html').pipe(res);
-  }
-
-
-  if(req.url === '/api'){
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var obj = {
-      firstname: 'Brian',
-      lastname: 'Sweeney'
-    }
-    res.end(JSON.stringify(obj))
-  }
-
-  res.writeHead(412);
-  res.end('I am a teapot'); 
-
-}).listen(1337, '127.0.0.1');
+var moment = require('moment');
+console.log(moment().format('ddd,hA'))
